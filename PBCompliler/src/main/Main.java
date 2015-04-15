ddc1455ebbbc59fcb36d26b11a106f71195468de
Debora <debymontinfo@gmail.com>
2015-04-15 00:32:27 -0300

    modificcação na class main

--- PBCompliler/src/main/Main.java
+++ PBCompliler/src/main/Main.java
@@ -1,31 +1,14 @@
 package main;
 
-import java.io.*;
-
-import syntaxtree.Program;
-import visitor.PrettyPrintVisitor;
+import syntaxtree.*;
+import visitor.*;
 import analizer.MiniJavaParser;
 import analizer.ParseException;
-import analizer.TokenMgrError;
 
 public class Main {
-	public static void main(String args[]) throws ParseException {
-		MiniJavaParser parser;
-		try {
-			parser = new MiniJavaParser(new FileInputStream(
-					"E:/ArquivoFonteTeste.txt"));
-			Program root = parser.parse();
-			root.accept(new PrettyPrintVisitor());
-			System.out.println("Successful!");
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + args[0]);
-			return;
-		} catch (TokenMgrError e) {
-			System.out.println("Fail! TokenMgrError: " + e);
-			return;
-		} catch (Error e) {
-			System.out.println("Fail! Error: " + e);
-			return;
-		}
-	}
+   public static void main(String [] args) throws ParseException {
+      MiniJavaParser parser = new MiniJavaParser(System.in);
+      Program root = parser.parse();
+      root.accept(new PrettyPrintVisitor());
+   }
 }
