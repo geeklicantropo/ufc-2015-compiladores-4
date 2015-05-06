d15f40575e446110985e2ad1355d5b3e6f208398
Debora <debymontinfo@gmail.com>
2015-05-06 00:08:16 -0300

    ultimo commit dos packages Frame e Mips

---
+++ PBCompliler/src/Assem/OPER.java
@@ -0,0 +1,11 @@
+package Assem;
+
+import Temp.Temp;
+
+public class OPER extends Instr {
+
+	// o ultimo Temp t nao eh Temp, mas nao sei o q deveria ser
+    public OPER(String a, Temp[] d, Temp[] s, Temp t) {
+    	//TODO
+    }
+}
